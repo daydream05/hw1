57d5143fa915bb3dd0b73fedb93d2c5365036dea
Vince P <ianparulan@gmail.com>
2017-02-22 18:19:30 -0500

    initial commit

---
+++ constructor.js
@@ -0,0 +1,7 @@
+function ObjectName(param1, param2) {
+    this.param1 = param1;
+    this.param2 = param2;
+    this.toString = function () {
+        return this.param1 + " " + this.param2;
+    };
+}
